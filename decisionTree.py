7bff674c63e9858e58b6f3783fa629642a3dce10
UnbiddenDragon <austenstevens1997@gmail.com>
2024-11-10 08:29:56 +0000

    Node class implemented

--- decisionTree.py
+++ decisionTree.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import Counter
+
 def entropy(y):
     counts = np.bincount(y)
     percentages = counts / len(y)
@@ -10,3 +11,21 @@ def entropy(y):
             entropy += p*np.log2(p)
         return -entropy
 
+class Node:
+    """
+    Helper class holding node informatin.
+
+    Arguments:
+        feature -- which column the threshold will refer to
+        threshold -- the splitting parameter
+        left -- left child node
+        right -- right child node
+        value -- label of the node, exist only if it is a leaf node
+    """
+
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
